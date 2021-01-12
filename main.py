c295247347fc02efcc57ed400fda248191d894ff
Kylie Phillips <kyjnik@gmail.com>
2021-01-12 22:17:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+def chat_bot():
+
+  responses = ("Hello" , "Sounds fun!", "that is great", "Okay bye have a great day")
+  return random.choice(responses)
+  print (random.choice(responses))
+name = input("what is your name ")
+input( chat_bot)
+
+hobby = input("what do you like to do ")
+input(chat_bot)
+well_being = input("how are you ")
+input(chat_bot)
+
+
+
+
+
+
+
